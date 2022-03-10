007185cdc4dea79466182a0e4b28e297b3633d15
Freivel Hirujo <freivelhv@hotmail.com>
2022-03-10 12:31:18 -0400

    dandole forma al crud

--- cuentasxpagar/cxpagar/urls.py
+++ cuentasxpagar/cxpagar/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
     path('conceptos', views.conceptos, name='conceptos'),
+    path('conceptos/crear', views.crear_concepto, name='crear_con'),
+    path('conceptos/editar', views.editar_concepto, name='editar_con'),
 ]
--- cuentasxpagar/cxpagar/views.py
+++ cuentasxpagar/cxpagar/views.py
@@ -5,4 +5,10 @@ def inicio(request):
     return HttpResponse("<h1>Bienvenido a Cuentas por Pagar</h1>")
 
 def conceptos(request):
-    return render(request, 'paginas/conceptos.html')    
+    return render(request, 'conceptos/index.html')   
+
+def crear_concepto(request):
+    return render(request, 'conceptos/crear.html')   
+
+def editar_concepto(request):
+    return render(request, 'conceptos/editar.html')  
