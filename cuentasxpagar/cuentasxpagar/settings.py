b86e97eac2ddd0fc2c2e9e87a7399e63b7bb7848
juniormena <juniormelpelotero@hotmail.com>
2022-04-09 01:48:51 -0400

    new settings

--- cuentasxpagar/cuentasxpagar/settings.py
+++ cuentasxpagar/cuentasxpagar/settings.py
@@ -53,6 +53,11 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
 ROOT_URLCONF = 'cuentasxpagar.urls'
 
 TEMPLATES = [
