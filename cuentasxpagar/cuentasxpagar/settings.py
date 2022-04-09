75598a375d840a4dc098ca03e3c255b8447db519
juniormena <juniormelpelotero@hotmail.com>
2022-04-09 01:28:01 -0400

    New things

--- cuentasxpagar/cuentasxpagar/settings.py
+++ cuentasxpagar/cuentasxpagar/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cuentasxpagar.urls'
@@ -71,7 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'cuentasxpagar.wsgi.application'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
